3048ab3f5952bb54a0b88333774e3a71d6ddb0de
Seth Lessard <seth.lessard@pm.me>
2020-02-02 14:59:34 -0600

    Remove the DeviceManager

--- src/manager/device/HomeConfigManager.js
+++ src/manager/device/HomeConfigManager.js
@@ -1,5 +1,5 @@
-import ConnectionBufferDatabase, { getConnectionBufferDatabaseInstance } from "../../db/ConnectionBufferDatabase";
-import DeviceManager, { getDeviceManagerInstance } from "./DeviceManager";
+import rp from "request-promise";
+import { getDeviceDatabaseInstance } from "../../db/DeviceDatabase";
 
 const ADDRESS = process.env.ADDRESS;
 const PORT = process.env.PORT;
@@ -11,31 +11,45 @@ let instance = null
  */
 const getHomeConfigManagerInstance = () => {
   if (instance == null) {
-    instance = new HomeConfigManager(getConnectionBufferDatabaseInstance(), getDeviceManagerInstance());
+    instance = new HomeConfigManager(getDeviceDatabaseInstance());
   }
   return instance;
 }
 
 class HomeConfigManager {
 
-  /**
-   *
-   * @param {ConnectionBufferDatabase} connectionBufferDatabase
-   * @param {DeviceManager} deviceManager
-   */
-  constructor(connectionBufferDatabase, deviceManager) {
-    this._connectionBufferDatabase = connectionBufferDatabase;
-    this._deviceManager = deviceManager
+  constructor(deviceDatabase) {
+    this._deviceDB = deviceDatabase;
 
     // TODO: binding
-    this._checkForConnectionInfo = this._checkForConnectionEntries.bind(this);
+    this._checkForDevicesToConfigure = this._checkForDevicesToConfigure.bind(this);
   }
 
   /**
-   * Initialize the connection manager and start looking for devices to connect.
+   * Start looking for devices to configure.
    */
-  initialize() {
-    this._connectionTimer = setInterval(() => this._checkForConnectionEntries(), 20000);
+  start() {
+    this._configurationTimer = setInterval(() => this._checkForDevicesToConfigure(), 15000);
+  }
+
+  /**
+   * Stop looking for devices.
+   */
+  stop() {
+    clearInterval(this._configurationTimer);
+  }
+
+  /**
+   * Set that a device has connected to the hub.
+   * @param {string} usn the device's usn.
+   */
+  setDeviceHasConnected(usn) {
+    return this._deviceDB.get(usn)
+    .then(iotDevice => {
+      iotDevice.status.connectedToHub = true;
+      iotDevice.status.lastConnectedToHub = Date.now();
+      return this._deviceDB.update(iotDevice);
+    });
   }
 
   /**
@@ -44,75 +58,42 @@ class HomeConfigManager {
    * @returns {Promise<void>}
    */
   setDeviceHasDisconnected(usn) {
-    this._deviceManager.getDeviceByUsn(usn)
-    .then(iotDevice => this._connectionBufferDatabase.insert({ usn, timeAdded: Date.now(), ipAddress: iotDevice.getAddress() }));
+    return this._deviceDB.get(usn)
+    .then(iotDevice => {
+      iotDevice.status.connectedToHub = false;
+      iotDevice.status.lastConnectedToHub = Date.now();
+      return this._deviceDB.update(iotDevice);
+    });
   }
 
   /**
-   * Check to see if there is any connection information in the database. If there is,
-   * attempt to reconnect the device.
-   * @returns {Promise<void>}
+   * Check to see if there are any devices that are disconnected and attempt to
+   * reconnect them to the Hub.
+   * @returns {Promise<void[]>}
    */
-  _checkForConnectionEntries() {
-    this._connectionBufferDatabase.getAll()
-    .then(connections => {
-      if (connections.length > 0) {
-        return this._connectDevicesToHub(connections)
-        .then(() => this._setDevicesReconnecting(connections));
-      }
-    })
+  _checkForDevicesToConfigure() {
+    return this._deviceDB.getAllUnconfiguredForHub()
+    .then(unconfigured => Promise.all(unconfigured.map(device => this._configureDeviceForHub(device.network.ipAddress))));
   }
 
   /**
-   * Connect a device to the Home.
+   * Connect a device to the Home Hub.
    * @param {string} ipAddress the ip address of the device.
    * @returns {Promise<void>}
    */
-  _connectDeviceToHome(ipAddress) {
-    return new Promise((resolve, reject) => {
-      const url = `http://${ipAddress}/config_parent`;
-      const body = JSON.stringify({
+  _configureDeviceForHub(ipAddress) {
+    const request = {
+      uri: `http://${ipAddress}/config_parent`,
+      body: JSON.stringify({
         parent: {
           address: ADDRESS,
           port: PORT
         }
-      });
-      request(url, { method: "POST", body }, (err, response, body) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve();
-      });
-    });
-  }
-
-  _getDisconnectedDevicesThatAreNotReconnecting() {
-    return this._deviceManager.getAllDevices() // TODO: do this filtering at the database level
-    .then(iotDevices => {
-
-    });
-  }
-
-  /**
-   * Connnect devices to this Hub
-   * @param {{ usn: string, timeAdded: number, ipAddress: string }[]} connectionInfos the connection info for the devices.
-   * @returns {Promise<void>}
-   */
-  _connectDevicesToHub(connectionInfos) {
-    // TODO: filter connectionInfos for devices whose connectionStatus is not "reconnecting"
-    return Promise.all(map(({ ipAddress }) => this._connectDeviceToHome(ipAddress)));
-  }
-
-  /**
-   *
-   * @param {{ usn: string, timeAdded: number, ipAddress: string}[]} connectionInfos the connection info for the devices.
-   */
-  _setDevicesReconnecting(connectionInfos) {
-    return Promise.all(connectionInfos.map(({ usn }) => this._deviceManager.getDeviceByUsn(usn).then(iotDevice => {
-      iotDevice.setConnectionStatus("reconnecting");
-      return this._deviceManager.updateDevice(iotDevice.toJson());
-    })));
+      }),
+      method: "POST"
+    };
+    return rp(request)
+    .then(body => {});
   }
 }
 
