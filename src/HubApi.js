816f1878409284bdf4f557e718427ab2b6f788e8
Seth Lessard <seth.lessard@pm.me>
2020-02-01 19:20:21 -0600

    Remove the WifiManager

--- src/HubApi.js
+++ src/HubApi.js
@@ -4,9 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 import DeviceRoutes from "./routes/Device";
-import WifiRoutes from "./routes/Wifi";
 import { getSsdpSearchManagerInstance } from "./manager/SsdpSearchManager";
-import { getWifiManagerInstace } from "./manager/WifiManager";
 
 class HubApi {
 
@@ -18,7 +16,6 @@ class HubApi {
 
     this._configureApi();
     this._SsdpSearchManager = getSsdpSearchManagerInstance();
-    this._wifiManager = getWifiManagerInstace();
   }
 
   /**
@@ -30,9 +27,6 @@ class HubApi {
     return new Promise((resolve, reject) => {
       this._server = this._api.listen(port, host, () => resolve());
       this._SsdpSearchManager.startListening();
-
-      this._wifiManager.initialize();
-      this._wifiManager.startListening();
     });
   }
 
@@ -43,7 +37,6 @@ class HubApi {
   stop() {
     return new Promise((resolve, reject) => {
       this._SsdpSearchManager.stopListening();
-      this._wifiManager.stopListening();
       this._server.close(err => {
         if (err) reject(err);
         else resolve();
@@ -57,7 +50,6 @@ class HubApi {
     this._api.use(bodyParser.json());
 
     this._api.use("/device", DeviceRoutes);
-    this._api.use("/wifi", WifiRoutes);
     this._api.use((req, res, next) => {
       res.status(404).send("Not found.");
     });
