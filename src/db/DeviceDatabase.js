ebd9b27a72e8ff64276cced95620eed27a82bec6
Seth Lessard <seth.lessard@pm.me>
2020-02-02 15:00:14 -0600

    Get all unconfigured devices

--- src/db/DeviceDatabase.js
+++ src/db/DeviceDatabase.js
@@ -11,40 +11,40 @@ const getDeviceDatabaseInstance = () => {
   return instance;
 }
 
-/**
- * {
- *    _id: string,
- *    usn: string,
- *    name: string,
- *    room: string,
- *    ssdp: {
- *      descriptionLocation: string,
- *      port: number
- *    },
- *    network: {
- *      addressFamily: string,
- *      ipAddress: string
- *    },
- *    services: [
- *      { name: string, version: string }
- *    ],
- *    serviceStatuses: [
- *      { name: string, version: string, status: string, updated: number }
- *    ],
- *    status: {
- *      configuredForHub: boolean,
- *      lastConfiguredForHub: number,
- *      connectedToHub: boolean,
- *      lastConnectedToHub: number,
- *      onlineOnNetwork: boolean,
- *      lastSeenOnNetwork: number
- *    },
- * }
- */
 class DeviceDatabase extends Database {
 
   /**
+   * Device Database.
    *
+   * Model:
+   * {
+   *    _id: string,
+   *    usn: string,
+   *    name: string,
+   *    room: string,
+   *    ssdp: {
+   *      descriptionLocation: string,
+   *      port: number
+   *    },
+   *    network: {
+   *      addressFamily: string,
+   *      ipAddress: string
+   *    },
+   *    services: [
+   *      { name: string, version: string }
+   *    ],
+   *    serviceStatuses: [
+   *      { name: string, version: string, status: string, updated: number }
+   *    ],
+   *    status: {
+   *      configuredForHub: boolean,
+   *      lastConfiguredForHub: number,
+   *      connectedToHub: boolean,
+   *      lastConnectedToHub: number,
+   *      onlineOnNetwork: boolean,
+   *      lastSeenOnNetwork: number
+   *    },
+   * }
    * @param {{ isMemoryDB?: boolean, isTest?: boolean }} options
    */
   constructor(options) {
@@ -52,6 +52,15 @@ class DeviceDatabase extends Database {
       name: "OpenHomeIoTDevices",
       isLedger: false
     }, options || {});
+  }
+
+  /**
+   * Get all devices that are not configured for the Hub.
+   * @returns {Promise<any[]>} // TODO: device type
+   */
+  getAllUnconfiguredForHub() {
+    return this.getAll()
+    .then(iotDevices => iotDevices.filter(iotDevice => iotDevice.status.configuredForHub === false));
   }
 }
 
